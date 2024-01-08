12025cfd0eac3f628da98732b17fd1d67fca2429
martin-ngigi <martinwainaina001@gmail.com>
2024-01-08 08:36:51 +0300

    Initial commit

---
+++ UberSwift/UberSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftApp.swift
+//  UberSwift
+//
+//  Created by Martin Wainaina on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
